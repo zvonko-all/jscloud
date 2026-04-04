8e649530ba1945a42964af53caed268d8fc50cb9
zvonko-all <115907075+zvonko-all@users.noreply.github.com>
2026-04-04 13:22:38 +0800

    Update console log message in greenseer.js111111

--- greenseer.js
+++ greenseer.js
@@ -1,5 +1,5 @@
 /*! ctu-greenseer 2022-12-29 13:39:25 71bb5cf2da5581ff46cade41689191a35e2381ef */
-console.log("============== hello zephan ==================")
+console.log("============== hello zephanxxxxxxx ==================")
 !function (n, e, r, t, o, i, a, c) {
   !function (u) {
     function s(r) {
